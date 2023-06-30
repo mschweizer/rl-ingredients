a50986cfb3254c0d9f443cbe8de0d4eeb91ee823
marvin.schweizer <marvin.schweizer@kit.edu>
2023-06-30 17:41:23 +0200

    Adds env_id param for vec_env creation

--- rl_ingredients/environment.py
+++ rl_ingredients/environment.py
@@ -10,18 +10,18 @@ env_ingredient = Ingredient("environment")
 # noinspection PyUnusedLocal
 @env_ingredient.config
 def cfg():
-    name = "CartPole-v0"  # the name of the task environment (must be registered with gym)
+    env_id = "CartPole-v0"  # the name of the task environment (must be registered with gym)
     num_envs = 2  # the number of environments (vectorized environments are used)
 
 
 @env_ingredient.capture
-def create_env(name, env_kwargs=None):
-    return gym.make(name, **env_kwargs if env_kwargs else {})
+def create_env(env_id, env_kwargs=None):
+    return gym.make(env_id, **env_kwargs if env_kwargs else {})
 
 
 @env_ingredient.capture
-def create_vec_env(num_envs, vec_env_start_method="spawn"):
-    env = create_env()
+def create_vec_env(env_id, num_envs, vec_env_start_method="spawn"):
+    env = create_env(env_id)
     vec_env = vectorize_env(env, num_envs, vec_env_start_method)
     return vec_env, env
 
